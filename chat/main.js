2e0433a29e038e033bd84c39daacd653142bc0b5
ankitv4087 <ankitv4087@gmail.com>
2024-09-29 14:29:08 +0530

    chat

--- chat/main.js
+++ chat/main.js
@@ -1,3 +1,13 @@
+const usersList = $('#usersList');
+const thumbnailBgColors = ['orange', 'red', 'green', 'blue', 'purple', 'pink', 'black', 'darkblue'];
+const conversationThumbnail = $('.conversation-thumbnail');
+const conversationName = $('.conversation-name');
+const conversationBio = $('.conversation-bio');
+const messageTextInput = $('.message-text-input');
+const sendTextMessageBtn = $('.send-text-message');
+const msgBody = $('.msg-body>ul');
+
+
 const preloader = document.querySelector('#preloader');
 if (preloader) {
   window.addEventListener('load', () => {
@@ -7,7 +17,7 @@ if (preloader) {
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+import { getDatabase, ref, set, get, child, push } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAOgC8M2o5Wz8yl3f1wGQ8p_rUWXujckpw",
@@ -73,22 +83,218 @@ function loginUser(user) {
     open_chat();
 }
 
+
 function open_chat() {
     if (window.localStorage.getItem('user')) {
-        // alert('You are ready to start chatting with me.');
         $('#login').addClass('d-none');
         $('#chat').removeClass('d-none');
+        getUsersList().then((users) => {
+            usersList.html('');
+            Object.keys(users).forEach((key) => {
+                let color = thumbnailBgColors[Math.floor(Math.random() * thumbnailBgColors.length)];
+                usersList.append(`
+                    <a href="#" class="d-flex align-items-center open-conversation" data-uid="${users[key].uid}">
+                        <div class="flex-shrink-0">
+                            <img class="img-fluid user-thumbnail"
+                                src="${users[key].photoURL}"
+                                alt="user img" onerror="this.src='https://placehold.co/45x45/${color}/white?font=lora&text=${users[key].displayName.charAt(0)}'">
+                            <span class="active"></span>
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                            <h3>${users[key].displayName}</h3>
+                            <p>${users[key].email}</p>
+                        </div>
+                    </a>
+                `);
+            })
+        })
     }
     else {
-        // alert('Please login first.');
         $('#login').removeClass('d-none');
         $('#chat').addClass('d-none');
     };
 }
 open_chat();
 
+$('body').on('click','.open-conversation',async function(e){
+    e.preventDefault();
+    let _this = $(this);
+    let uid = $(this).data('uid');
+    let sender = JSON.parse(window.localStorage.getItem('user')).uid;
+    conversationThumbnail.attr('src', _this.find('img').attr('src'));
+    conversationName.text(_this.find('h3').text());
+    conversationBio.text('Some bio about ' + _this.find('h3').text() + '.');
+    messageTextInput.data('uid', uid);
+    // open_chat();
+    const conversationRefPath1 = 'conversations/' + sender + '-' + uid;
+    const conversationRefPath2 = 'conversations/' + uid + '-' + sender;
+
+    let conversationRef;
+    let conversationSnapshot;
+    try {
+        // Check if the first conversation path exists
+        conversationRef = ref(database, conversationRefPath1);
+        conversationSnapshot = await get(conversationRef);
+        // If it does not exist, check the second path
+        if (!conversationSnapshot.exists()) {
+            conversationRef = ref(database, conversationRefPath2);
+            conversationSnapshot = await get(conversationRef);
+        }
+    } catch (error) {
+        console.error(error);
+        alert('Something went wrong. Please try again later.');
+    }
+    conversationSnapshot = conversationSnapshot.val();
+    $.each(conversationSnapshot, (index, val) => {
+        console.log({ index, val })
+        let className = (val.sender === sender) ? 'sender' : 'repaly';
+        let message = val.message;
+        msgBody.append(`<li class="${className}">
+                            <p>${message}</p>
+                            <span class="time">10:35 am</span>
+                        </li>`);
+    })
+    // conversationSnapshot.each((val,index) => {
+    //     console.log({index,val})
+    // })      
+})
+
+sendTextMessageBtn.on('click', async function (e) {
+    e.preventDefault();
+    
+    const uid = messageTextInput.data('uid');
+    const sender = JSON.parse(window.localStorage.getItem('user')).uid;
+    const message = messageTextInput.val();
+
+    // Define potential conversation references
+    const conversationRefPath1 = 'conversations/' + sender + '-' + uid;
+    const conversationRefPath2 = 'conversations/' + uid + '-' + sender;
+    
+    let conversationRef;
+    let conversationRefPath;
+    try {
+        // Check if the first conversation path exists
+        conversationRef = ref(database, conversationRefPath1);
+        let conversationSnapshot = await get(conversationRef);
+        conversationRefPath = conversationRefPath1;
+        // If it does not exist, check the second path
+        if (!conversationSnapshot.exists()) {
+            conversationRef = ref(database, conversationRefPath2);
+            conversationRefPath = conversationRefPath2;
+        }
+
+        var conversationRefKey = push(child(conversationRef, 'conversations')).key;
+        conversationRef = ref(database, conversationRefPath+ '/' + conversationRefKey);
+
+
+        set(conversationRef, {
+            sender: uid,
+            message: message,
+            timestamp: Date.now()
+        })
+        .then(() => {
+            console.log('message sent');
+        })
+        .catch((error) => {
+            alert('Something went wrong. Please try again later.');
+        });
+        messageTextInput.val(''); // Clear the input field
+    } catch (error) {
+        console.error(error);
+        alert('Something went wrong. Please try again later.');
+    }
+});
+
+
+// sendTextMessageBtn.on('click',async function(e){
+//     e.preventDefault();
+//     let uid = messageTextInput.data('uid');
+//     let sender = JSON.parse(window.localStorage.getItem('user')).uid;
+//     let message = messageTextInput.val();
+
+//     let conversationRef = ref(database, 'conversations/' + sender + '-' + uid);
+//     var conversationSnapshot = await get(conversationRef);
+//     if (conversationSnapshot.exists()) {
+//         conversationRef = 'conversations/' + sender + '-' + uid;
+//     }
+//     else{
+//         conversationRef = 'conversations/' + sender + '-' + uid;
+//     }
+
+//     if (!conversationSnapshot.exists()) {
+//         conversationRef = ref(database, 'conversations/' + sender + '-' + uid);
+//         var conversationRefKey = push(child(conversationRef, 'conversations')).key;
+//     }
+//     else{
+//         var conversationRefKey = push(child(conversationRef, 'conversations')).key;
+//         conversationRef = ref(database, 'conversations/' + sender + '-' + uid + '/' + conversationRefKey);
+//     }
+    
+
+//     set(conversationRef, {
+//         sender: uid,
+//         message: message
+//     })
+//     .then(() => {
+//         console.log('message sent');
+//     })
+//     .catch((error) => {
+//         alert('Something went wrong. Please try again later.');
+//     });
+//     // let key = 
+//     // console.log(key);
+//         // conversationRef.push({
+//         //     uid: uid,
+//         //     message: message
+//         // })
+//         // set(conversationRef, {
+//         //     uid: uid,
+//         //     message: message
+//         // })
+//         // .then(() => {
+//         //     console.log('message sent');
+//         // })
+//         // .catch((error) => {
+//         //     alert('Something went wrong. Please try again later.');
+//         // });
+//     // messageTextInput.val('');
+
+//     // const conversationRef = ref(database, 'conversations');
+//     // const conversationSnapshot = await get(conversationRef);
+//     // if (conversationSnapshot.exists()) {
+//     //     console.log(conversationSnapshot.val());
+//     // }
+    
+// })
+
+
 $('body').on('click','#sign-out',function(e){
     e.preventDefault();
     window.localStorage.removeItem('user');
     window.location.reload();
 })
+
+
+
+
+
+
+async function getUsersList() {
+    const userRef = ref(database, 'users');
+    const userSnapshot = await get(userRef);
+    if (userSnapshot.exists()) {
+        return userSnapshot.val();
+    }
+}
+
+async function loadConversations(uid) {
+    const conversationRef = ref(database, 'conversations');
+    const conversationSnapshot = await get(conversationRef);
+    if (conversationSnapshot.exists()) {
+        return conversationSnapshot.val();
+    }
+    return false;
+}
+
+
+
